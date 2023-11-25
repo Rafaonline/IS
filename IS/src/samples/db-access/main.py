de48d73acf4388a90c3e1be7c21fd9f749659570
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2023-11-25 19:16:19 +0000

    Insert na db funcional

--- IS/src/samples/db-access/main.py
+++ IS/src/samples/db-access/main.py
@@ -1,29 +1,44 @@
 import psycopg2
 
-connection = None
-cursor = None
+
+class DatabaseConnection:
+    def __init__(self):
+        self.connection = psycopg2.connect(
+            user="is",
+            password="is",
+            host="is-db",
+            port="5432",
+            database="is"
+        )
+        self.cursor = self.connection.cursor()
+
+    def insert_xml_document(self, file_name, xml):
+        try:
+            self.cursor.execute(
+                "INSERT INTO imported_documents (file_name, xml) VALUES (%s, %s)",
+                (file_name, xml)
+            )
+            self.connection.commit()
+            print("Document inserted successfully!")
+        except Exception as exception:
+            print("Error inserting document: ", exception)
+
+        finally:
+            if self.connection:
+                self.cursor.close()
+                self.connection.close()
+
+
+# Assuming you have the XML content and file name ready
+with open("/data/retail.xml", "r", encoding="utf-8") as file:
+    xml = file.read()
+file_name = "retail5.xml"
+
+# Create an instance of the DatabaseConnection class
+db_connection = DatabaseConnection()
 
 try:
-    connection = psycopg2.connect(user="is",
-                                  password="is",
-                                  host="is-db",
-                                  port="5432",
-                                  database="is")
-
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT * FROM teachers")
-
-    print("DOcs list:")
-    for docs in cursor:
-        print(f" > {docs[0]}, from {docs[1]}")
-
-except (Exception, psycopg2.Error) as error:
-    print("Failed to fetch data", error)
-
-finally:
-    if connection:
-        cursor.close()
-        connection.close()
-
-
+    # Insert XML document into the database
+    db_connection.insert_xml_document(file_name, xml)
+except Exception as error:
+    print("Failed to insert data", error)
--- IS/src/samples/xml-generation/main.py
+++ IS/src/samples/xml-generation/main.py
@@ -2,5 +2,5 @@ from csv_to_xml_converter import CSVtoXMLConverter
 
 if __name__ == "__main__":
     converter = CSVtoXMLConverter("/data/Retail_Transactions_Dataset.csv")
-    converter.to_xml_file()
+    # converter.to_xml_file()
     # print(converter.to_xml_str())
