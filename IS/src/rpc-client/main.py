53e6a07040eac0b44dbc3976016ebd95f5489a87
novaisdiogo12 <diogorafaelsantosnovais@gmail.com>
2023-11-26 14:26:36 +0000

    Menu

--- IS/src/rpc-client/main.py
+++ IS/src/rpc-client/main.py
@@ -1,9 +1,70 @@
 import xmlrpc.client
+import xml.etree.ElementTree as ET
 
 print("connecting to server...")
 server = xmlrpc.client.ServerProxy('http://is-rpc-server:9000')
 
-string = "hello world"
 
-print(f" > {server.string_reverse(string)}")
-print(f" > {server.string_length(string)}")
+def menu():
+    print("1-Consultar")
+    print("2-Pesquisar")
+    print("3-Sair")
+
+def consultar():
+    print("1-Lojas")
+    print("2-Cidades")
+    print("3-Tipos de loja")
+    print("4-Categoria de clientes")
+    print("5-Voltar")
+
+def pesquisar():
+    print("1- Id de transacao")
+    print("2- Estacao do ano")
+    print("3- Cidade da loja")
+    print("4- Voltar")
+
+
+
+
+while True:
+    menu()
+
+    escolha = input("Escolha a opcao: ")
+
+    if escolha == "1":
+        consultar()
+        escolha_consulta = input("Escolha a opcao")
+
+        if escolha_consulta == "1":
+            """
+            # Chamar a função remota para consultar lojas
+            lojas = server.consultar_lojas()
+
+            # Exibir as informações das lojas
+            if lojas:
+                for loja in lojas:
+                    print(f"ID da Transação: {loja['transaction_id']}, ID da Loja: {loja['store_id']}")
+            else:
+                print("Nenhuma loja encontrada.")
+            """
+        #if escolha_consulta == "2":
+
+        #if escolha_consulta == "3":
+
+        #if escolha_consulta == "4":
+
+        if escolha_consulta == "5":
+            menu()
+
+    elif escolha == "2":
+        pesquisar()
+        escolha_pesquisa = input("Escolha a opcao")
+
+        #if escolha_pesquisa == "1":
+
+       # if escolha_pesquisa == "2":
+
+        #if escolha_pesquisa == "3":
+
+        if escolha_pesquisa == "4":
+            menu()
--- IS/src/rpc-server/main.py
+++ IS/src/rpc-server/main.py
@@ -1,15 +1,36 @@
 import signal, sys
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
-
+import xml.etree.ElementTree as ET
 from functions.string_length import string_length
 from functions.string_reverse import string_reverse
 
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
+"""
+def consultar_lojas():
+    idloja = '/data/retail.xml'
+    lojas = lojasxml(idloja)
 
+    return lojas
 
+def lojasxml(idloja):
+    tree = ET.parse(idloja)
+    root = tree.getroot()
+
+    lojas = []
+
+    for transaction_elem in root.findall('.//Transaction'):
+        transaction_id = transaction_elem.get('ID')
+        store_elem = transaction_elem.find('./Store')
+        store_id = store_elem.get('ID') if store_elem is not None else None
+
+        if store_id is not None:
+            lojas.append({'transaction_id': transaction_id, 'store_id': store_id})
+
+    return lojas
+"""
 with SimpleXMLRPCServer(('0.0.0.0', 9000), requestHandler=RequestHandler) as server:
     server.register_introspection_functions()
 
